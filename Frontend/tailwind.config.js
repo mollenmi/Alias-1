1e52f098a4735f20890663d8da9bc1baaf2a9bd0
Navneet Kushwaha <117534965+mollenmi@users.noreply.github.com>
2024-12-28 21:49:34 +0530

    correction

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', 'sans-serif'],
+      }
+    },
   },
   plugins: [require('daisyui'),],
 }
